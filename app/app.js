b5e42dcbdc0c72cdd26dbcf4ac3abfea23a7fed8
Gabe <gman3546@gmail.com>
2017-10-14 22:06:48 -0500

    made unnecssary changes to app.js

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname + '/public')))
 
 /* ROUTES */
 app.get('/', function(req, res) {
-    res.send('Hello, world');
+//    res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 app.get('/test', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/test.html'))
